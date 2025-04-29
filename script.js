9e46388b4424ddae785e738e455ba158ef26d039
k_rajendra <31775586+rajendrakujur@users.noreply.github.com>
2025-04-29 23:12:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // Typed.js effect
 document.addEventListener("DOMContentLoaded", () => {
   new Typed(".typing", {
-    strings: ["Web Developer", "Open Source Contributor", "UI/UX Designer"],
+    strings: ["Web Developer", "UI/UX Designer", "Open Source Contributor"],
     typeSpeed: 80,
     backSpeed: 40,
     loop: true
+  });
+
+  // Dark mode toggle
+  const toggle = document.getElementById("darkToggle");
+  toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
   });
 });
