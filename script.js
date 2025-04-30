5b307a802737df46a8ffa867fd00c8c89a9bf85a
k_rajendra <31775586+rajendrakujur@users.noreply.github.com>
2025-04-30 19:30:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // Typed.js effect
-document.addEventListener("DOMContentLoaded", () => {
-  new Typed(".typing", {
-    strings: ["Web Developer", "UI/UX Designer", "Open Source Contributor"],
-    typeSpeed: 80,
-    backSpeed: 40,
-    loop: true
-  });
-
-  // Dark mode toggle
-  const toggle = document.getElementById("darkToggle");
-
-  // Check if dark mode is enabled from localStorage
-  if (localStorage.getItem("dark-mode") === "enabled") {
-    document.body.classList.add("dark-mode");
-  }
-
-  toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-
-    // Store dark mode state in localStorage
-    if (document.body.classList.contains("dark-mode")) {
-      localStorage.setItem("dark-mode", "enabled");
-    } else {
-      localStorage.setItem("dark-mode", "disabled");
-    }
-  });
-});
+ document.addEventListener("DOMContentLoaded", () => {
+   new Typed(".typing", {
+     strings: ["Web Developer", "UI/UX Designer", "Open Source Contributor"],
+     typeSpeed: 80,
+     backSpeed: 40,
+     loop: true
+   });
+ 
+   // Dark mode toggle
+   const toggle = document.getElementById("darkToggle");
+ 
+   // Check if dark mode is enabled from localStorage
+   if (localStorage.getItem("dark-mode") === "enabled") {
+     document.body.classList.add("dark-mode");
+   }
+ 
+   toggle.addEventListener("click", () => {
+     document.body.classList.toggle("dark-mode");
+ 
+     // Store dark mode state in localStorage
+     if (document.body.classList.contains("dark-mode")) {
+       localStorage.setItem("dark-mode", "enabled");
+     } else {
+       localStorage.setItem("dark-mode", "disabled");
+     }
+   });
+ });
